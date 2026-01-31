148aa51cdfa8e008c90b975e31b294d55e7e7c27
KonradMarzecMyDr <konrad.marzec@docplanner.com>
2026-01-31 21:43:43 +0100

    add env

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 
 from fastapi import FastAPI
 
+APP_ENV = os.getenv("APP_ENV", "dev")
+LOG_LEVEL = os.getenv("LOG_LEVEL", "info")
 
 app = FastAPI()
 
@@ -19,3 +21,10 @@ def version():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/env")
+def env():
+    return {
+        "env": APP_ENV,
+        "log_level": LOG_LEVEL
+    }
